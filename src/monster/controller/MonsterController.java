2f21478a5c11619cb0c4b8de97623458343c4c9d
Amber Bullough <amber@bertbullough.com>
2017-09-15 09:13:51 -0600

    Added scanner and parameter

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,6 +1,7 @@
 package monster.controller;
 
 import monster.model.MarshmallowMonster;
+import java.util.Scanner;
 
 public class MonsterController 
 {
@@ -14,5 +15,13 @@ public class MonsterController
 		System.out.println("Vice is so hungry he is going to eat a tentacle!");
 		realMonster.setTentacleAmount(4);
 		System.out.println(realMonster);
+		System.out.println("Vice is so hungry he is going to eat another tentacle and two legs!");
+		realMonster.setTentacleAmount(3);
+		realMonster.setArmCount(3);
+		interactWithTheMonster(realMonster);
+	}
+	private void interactWithTheMonster(MarshmallowMonster currentMonster)
+	{
+		System.out.println();
 	}
 }
